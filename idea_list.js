9d56ab8377649267b1d2af3585e4c4ec194d3fe0
Liu Jiyang <2411mail@gmail.com>
2015-03-29 14:28:56 +0800

    add database

--- idea_list.js
+++ idea_list.js
@@ -1,3 +1,4 @@
+Ideas = new Mongo.Collection("ideas");
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -5,13 +6,21 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    abc: function () {
+      Ideas.findOne().score;
+      return Session.get('counter');
     }
   });
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
+      console.log("click hello template");
       Session.set('counter', Session.get('counter') + 1);
+      Ideas.insert({
+        score: Session.get('counter')
+      });
     }
   });
 }
@@ -19,5 +28,18 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    var names = [
+    "8小时做一个 facebook",
+    "8小时做一个 twitter",
+    "8小时做一个 知乎"
+    ];
+    _.each(names,function(name) { //underscorejs.org, javascipt
+      Ideas.insert({
+        name: name,
+        score: 0
+      })
+    })
   });
 }
+
+console.log("not in both client and server");
